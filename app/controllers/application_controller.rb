674a48778c807617bc3600bc13c25b588a654ea4
Shujaat Ali <shujaat222@gmail.com>
2023-02-23 19:52:41 +0500

    Completed UI of rooms and inbox of rooms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user = User.find(session[:user_id]) if session[:user_id].present?
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+    @current_user = user
+
+    redirect_to root_path
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'rooms#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+
+  resources :rooms do
+    resources :messages
+  end
+
+  resources :users
 end
